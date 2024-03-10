892dc7b533932f12e1320e6e6505ec24d051ff7a
Николай <kolya-91@list.ru>
2024-03-10 15:17:31 +0500

    Переделал, с учётом пожеланий

--- src/Main.java
+++ src/Main.java
@@ -23,8 +23,6 @@ public class Main {
             if (payment[i] > maxPayment) {
                 maxPayment = payment[i];
             }
-        }
-        for (int i = 0; i < payment.length; i++) {
             if (payment[i] < minPayment) {
                 minPayment = payment[i];
             }
@@ -42,13 +40,9 @@ public class Main {
         System.out.println(" Задача 4");
 
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = 0; i < reverseFullName.length / 2; i++) {
-            char temp = reverseFullName[i];
-            reverseFullName[i] = reverseFullName[reverseFullName.length - i -1];
-            reverseFullName[reverseFullName.length - i - 1] = temp;
-        }
-        for (int i = 0; i < reverseFullName.length; i++) {
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
+
     }
 }
